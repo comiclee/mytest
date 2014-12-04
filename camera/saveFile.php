e608e1983f6d85c20030f7497cb5b0750b1cbd5a
chenli08 <chenli08@meituan.com>
2014-12-04 17:46:36 +0800

    后端逻辑

--- camera/saveFile.php
+++ camera/saveFile.php
@@ -1,3 +1,10 @@
 <?php
-echo 'saved';
+if ($_FILES["pic"]["error"] > 0) {
+  echo "Error: " . $_FILES["pic"]["error"] . "<br />";
+} else {
+  echo "Upload: " . $_FILES["pic"]["name"] . "<br />";
+  echo "Type: " . $_FILES["pic"]["type"] . "<br />";
+  echo "Size: " . ($_FILES["pic"]["size"] / 1024) . " Kb<br />";
+  echo "Stored in: " . $_FILES["pic"]["tmp_name"];
+}
 ?>
